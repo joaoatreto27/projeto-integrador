32738f286785c3e1cbc2bf4f1f6a5458435c819f
Joao Treto <joaoatreto@gmail.com>
2023-03-10 20:34:13 -0300

    First commit

---
+++ scripts/index.js
@@ -0,0 +1,4 @@
+const inputFile = document.querySelector(".picture-input");
+const pictureImage = document.querySelector(".picture-image");
+const pictureImageTxt = "Escolha uma imagem para o produto";
+pictureImage.innerHTML = pictureImageTxt;
